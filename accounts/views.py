74beecf0469de350ecb7d0baece784b20a00be81
Adeeba Khan <kh.adeeba15@gmail.com>
2023-10-14 20:08:15 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def login(request):
+    ctx= {}
+    return render(request, 'accounts/login.html', ctx)
+
+def register(request):
+    ctx={}
+    return render(request, 'accounts/register.html', ctx)
+
